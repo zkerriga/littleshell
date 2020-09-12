b195216a086335e5b8bc5471226ca3f22d99c715
zkerriga <daniil.63@yandex.ru>
2020-09-12 17:24:28 +0300

    Add headers

--- includes/clear_command_line.h
+++ includes/clear_command_line.h
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   clear_command_line.h                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/12 15:08:28 by zkerriga          #+#    #+#             */
-/*   Updated: 2020/09/12 15:08:31 by zkerriga         ###   ########.fr       */
+/*   Created: 2020/09/12 17:23:47 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/09/12 17:23:50 by zkerriga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
 
-# include "libft.h"
-
-typedef struct	s_cmd
-{
-	char		*cmd_name;
-	char		**args;
-	char		**redir_in;
-	char		**redir_in_app;
-	char		**redir_out;
-	char		**redir_out_app;
-	char		next_operator[2];
-}				t_cmd;
-
-#endif
