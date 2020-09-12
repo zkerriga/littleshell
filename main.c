89e0825edb4293ce8f888b312036ad121a6dd977
zkerriga <daniil.63@yandex.ru>
2020-09-12 15:42:36 +0300

    Add gitignore and libft

--- main.c
+++ main.c
@@ -12,27 +12,27 @@
 
 int loop(char **envp)
 {
-    char    *cmd_line;
-    char    *clean_cmd_line;
-    t_list  *cmd_list;
-    int     status;
+	char	*cmd_line;
+	char	*clean_cmd_line;
+	t_list	*cmd_list;
+	int		status;
 
-    status = 1;
-    while (status)
-    {
-        ft_printf("$> ");
-        cmd_line = read_line();
-        clean_cmd_line = clear_comand_line(cmd_line);
-        cmd_list = parse_comands(clean_cmd_line);
-        status = exec_all_comands(cmd_list);
-        free(cmd_line);
-        free(clean_cmd_line);
-        ft_list_clear(cmd_list);
-    }
+	status = 1; //TODO: сделать штуку, которая отчистит терминал
+	while (status)
+	{
+		write(1, "$> ", 3);
+		cmd_line = read_line();
+		clean_cmd_line = clear_comand_line(cmd_line);
+		cmd_list = parse_comands(clean_cmd_line);
+		status = exec_all_comands(cmd_list);
+		free(cmd_line);
+		free(clean_cmd_line);
+		ft_list_clear(cmd_list);
+	}
 }
 
 int main(int ac, char **av, char **envp)
 {
-    loop(envp);
+	loop(envp);
 	return (0);
 }
