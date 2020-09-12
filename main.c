7875b5a1e7faa221ae88cf9f6cf21ad6e158f35a
zkerriga <daniil.63@yandex.ru>
2020-09-12 15:24:32 +0300

    Add start files

---
+++ main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/12 14:58:02 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/09/12 14:58:06 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int loop(char **envp)
+{
+    char    *cmd_line;
+    char    *clean_cmd_line;
+    t_list  *cmd_list;
+    int     status;
+
+    status = 1;
+    while (status)
+    {
+        ft_printf("$> ");
+        cmd_line = read_line();
+        clean_cmd_line = clear_comand_line(cmd_line);
+        cmd_list = parse_comands(clean_cmd_line);
+        status = exec_all_comands(cmd_list);
+        free(cmd_line);
+        free(clean_cmd_line);
+        ft_list_clear(cmd_list);
+    }
+}
+
+int main(int ac, char **av, char **envp)
+{
+    loop(envp);
+	return (0);
+}
