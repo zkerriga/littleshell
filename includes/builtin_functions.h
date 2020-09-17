52b0ff79d3bd5055c5d1de58492e7d09b017e376
zkerriga <daniil.63@yandex.ru>
2020-09-17 11:39:03 +0300

    Add pwd

--- includes/builtin_functions.h
+++ includes/builtin_functions.h
@@ -25,6 +25,7 @@ int		export_command(char **args, int fdin, int fdout, t_env *env);
 int		exit_command(char **args, int fdin, int fdout, t_env *env);
 int		echo_command(char **args, int fdin, int fdout, t_env *env);
 int		cd_command(char **args, int fdin, int fdout, t_env *env);
+int		pwd_command(char **args, int fdin, int fdout, t_env *env);
 
 void	write_err(char *cmd_name, char *trouble_str, char *description);
 
