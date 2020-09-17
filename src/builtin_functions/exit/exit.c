e95f2019a842a15703d0bc4776e354cfa31f575e
zkerriga <daniil.63@yandex.ru>
2020-09-17 08:55:22 +0300

    Add basix exit

---
+++ src/builtin_functions/exit/exit.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 08:10:04 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/09/17 08:10:05 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "environment.h"
+#include "builtin_functions.h"
+
+static int	is_numeric_argument(char *arg)
+{
+	while (ft_isspace(*arg))
+		++arg;
+	if (*arg == '-' || *arg == '+')
+		++arg;
+	while (*arg)
+	{
+		if (!ft_isdigit(*arg))
+			return (0);
+		++arg;
+	}
+	return (1);
+}
+
+int		exit_command(char **args, int fdin, int fdout, t_env *env)
+{
+	const int	exit_error_code = 1;
+	const int	alpha_error_code = 255;
+	int			unused;
+	int			status;
+
+	unused = fdout || fdin;
+	write(2, "exit\n", 6);
+	if (!args[1])
+	{
+		exit(env->get_status(env));
+	}
+	else if (is_numeric_argument(args[1]))
+	{
+		if (args[2])
+		{
+			write_err("exit", NULL, "too many arguments");
+			return (errno = exit_error_code);
+		}
+		exit((unsigned char)ft_atoi(args[1]));
+	}
+	return (0);
+}
