2081f493c77b52fcfe06e5f2524c5ff91f310c91
zkerriga <daniil.63@yandex.ru>
2020-09-18 18:27:01 +0300

    Add error function

---
+++ includes/error_manager.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_manager.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 18:08:43 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/09/18 18:08:44 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_MANAGER_H
+# define ERROR_MANAGER_H
+
+# include <errno.h>
+# include <unistd.h>
+# include <string.h>
+
+int		errman(int error_code, const char *description);
+
+#endif
