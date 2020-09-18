a97c91c38da19f1bb4d5888d1ad4626d228f0f22
zkerriga <daniil.63@yandex.ru>
2020-09-18 18:27:01 +0300

    Add error function

---
+++ src/errors/error_manager.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_manager.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 18:08:57 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/09/18 18:08:58 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "error_manager.h"
+#include "libft.h"
+
+void	write_error_message(int error_code, const char *description)
+{
+	write(2, "minishell: ", 11);
+	description = (description) ? description : strerror(error_code);
+	write(2, description, ft_strlen(description));
+	write(2, "\n", 1);
+}
+
+int		errman(int error_code, const char *description)
+{
+	int		status;
+
+	status = 0;
+	write_error_message(error_code, description);
+	if (error_code == ENOMEM)
+		exit(ENOMEM);
+	return (status);
+}
