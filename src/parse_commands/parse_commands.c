4bbd5b8a091f8959d40f09ab71f670428cc2b3d4
zkerriga <daniil.63@yandex.ru>
2020-09-19 09:54:50 +0300

    renames

--- src/parse_commands/parse_commands.c
+++ src/parse_commands/parse_commands.c
@@ -10,28 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "libft.h"
 #include "minishell.h"
 #include "parse_commands.h"
 #include "environment.h"
-
-void	destroy_command(t_command *cmd)
-{
-	if (cmd->redir_out_app)
-		ft_free_tab((void **)cmd->redir_out_app);
-	if (cmd->redir_out)
-		ft_free_tab((void **)cmd->redir_out);
-	if (cmd->redir_in)
-		ft_free_tab((void **)cmd->redir_in);
-	if (cmd->redir_in_app)
-		ft_free_tab((void **)cmd->redir_in_app);
-	if (cmd->args)
-		ft_free_tab((void **)cmd->args);
-	if (cmd->cmd_name)
-		free(cmd->cmd_name);
-	ft_bzero(cmd, sizeof(*cmd));
-}
 
 /*
 **	Parse command from *cmd_line till separate char (';' or '|'), then
